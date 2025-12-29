66bbcb741a53366a03d8b9d9f0dad1f538e55c08
Samual Molla <samualmolla16@gmail.com>
2025-12-30 00:37:21 +0100

    final work

--- amazon-clone/src/components/Category/categoryList.js
+++ amazon-clone/src/components/Category/categoryList.js
@@ -1,7 +1,7 @@
-import electronicsImg from "./img/electronics.jpeg";
+import electronicsImg from "./img/electronics.jpg";
 import jewelryImg from "./img/jewelry.jpeg";
 import mensClothingImg from "./img/mens-clothing.jpeg";
-import womensClothingImg from "./img/womens-clothing.jpeg";
+import womensClothingImg from "./img/womens-clothing.jpg";
 
 export const categories = [
   { title: "Electronics", name: "electronics", image: electronicsImg },
