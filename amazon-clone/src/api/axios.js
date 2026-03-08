12c1c12aa6f0c603d1907d64c1cf19dae5fe773a
Samual Molla <samualmolla16@gmail.com>
2026-03-08 13:50:06 +0100

    update

--- amazon-clone/src/api/axios.js
+++ amazon-clone/src/api/axios.js
@@ -4,5 +4,6 @@ const axiosInstance = axios.create({
   // Backend is running on HTTP port 5001
   //baseURL: "http://localhost:5001",
   baseURL: "https://api-gtovj2gsgq-uc.a.run.app",
+  //baseURL:"https://amazon-api-deploy-81p7.onrender.com",
 });
 export {axiosInstance};
